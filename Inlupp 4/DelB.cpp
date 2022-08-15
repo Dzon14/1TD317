4234b018b3146be52dca98540af691ede63f0abc
Dzon14 <daniel@ddc.se>
2022-08-15 15:23:02 +0200

    Inl4 done

--- Inlupp 4/DelB.cpp
+++ Inlupp 4/DelB.cpp
@@ -5,92 +5,201 @@
 #include <cmath>
 using namespace std;
 
-//Globala konstanter
-const int ANTAL_BOKSTAVER = 26;  //A-Z
+// Globala konstanter
+const int ANTAL_BOKSTAVER = 26; // A-Z
 const int ANTAL_SPRAK = 4;
 
-const double TOLK_HJALP[ANTAL_SPRAK][ANTAL_BOKSTAVER]=
-       {{8.27,1.48,2.94,4.03,11.78,2.22,1.72,6.77, //engelska
-         7.39,0.12,0.81,3.76,2.85,6.71,7.79,1.54,
-         0.05,5.95,6.69,9.07,2.66,1.13,2.14,0.19,
-         1.89,0.03},
-        {7.97,1.40,3.55,3.79,16.89,1.02,1.00,0.75, //franska
-         7.08,0.38,0.04,5.51,2.82,8.11,5.19,2.78,
-         1.01,6.69,8.35,7.22,6.09,1.35,0.02,0.54,
-         0.30,0.15},
-        {9.50,1.11,1.53,5.30,8.94,1.74,3.57,3.94,  //svenska
-         3.98,0.89,3.26,4.93,3.41,8.46,5.01,1.77,
-         0.00,6.73,5.56,9.20,1.94,2.42,0.00,0.05,
-         0.45,0.00},
-        {5.12,1.95,3.57,5.07,16.87,1.35,3.00,5.79, //tyska
-         8.63,0.19,1.14,3.68,3.12,10.64,1.74,0.42,
-         0.01,6.30,6.99,5.19,3.92,0.77,1.79,0.01,
-         0.69,1.24}};
+const double TOLK_HJALP[ANTAL_SPRAK][ANTAL_BOKSTAVER] =
+    {{8.27, 1.48, 2.94, 4.03, 11.78, 2.22, 1.72, 6.77, // engelska
+      7.39, 0.12, 0.81, 3.76, 2.85, 6.71, 7.79, 1.54,
+      0.05, 5.95, 6.69, 9.07, 2.66, 1.13, 2.14, 0.19,
+      1.89, 0.03},
+     {7.97, 1.40, 3.55, 3.79, 16.89, 1.02, 1.00, 0.75, // franska
+      7.08, 0.38, 0.04, 5.51, 2.82, 8.11, 5.19, 2.78,
+      1.01, 6.69, 8.35, 7.22, 6.09, 1.35, 0.02, 0.54,
+      0.30, 0.15},
+     {9.50, 1.11, 1.53, 5.30, 8.94, 1.74, 3.57, 3.94, // svenska
+      3.98, 0.89, 3.26, 4.93, 3.41, 8.46, 5.01, 1.77,
+      0.00, 6.73, 5.56, 9.20, 1.94, 2.42, 0.00, 0.05,
+      0.45, 0.00},
+     {5.12, 1.95, 3.57, 5.07, 16.87, 1.35, 3.00, 5.79, // tyska
+      8.63, 0.19, 1.14, 3.68, 3.12, 10.64, 1.74, 0.42,
+      0.01, 6.30, 6.99, 5.19, 3.92, 0.77, 1.79, 0.01,
+      0.69, 1.24}};
 
 // Funktionsdeklarationer:
 
-void berakna_histogram_abs(const string& in, int frekvens[], int &antal);
+void berakna_histogram_abs(const string &in, int frekvens[], int &antal);
 void abs_till_rel(int frekvens[], double relativFrekvens[], int &antal);
 void plotta_historgram_rel(int frekvens[], double relativFrekvens[]);
 void tolkning(double relativFrekvens[]);
-string namn_pa_fil();
-string inlasning(string filnamn);
+void namn_pa_fil(string &filnamn);
+void inlasning(string filnamn, string &text);
 
 // Huvudprogram:
 int main() {
     int frekvens[ANTAL_BOKSTAVER];
     double relativFrekvens[ANTAL_BOKSTAVER];
-    //string fil;
-    //string text;
+    string filnamn;
+    string text;
     int antal;
 
-
-    string filnamn = namn_pa_fil();
-    string text = inlasning(filnamn);
+    //Läser in fil
+    namn_pa_fil(filnamn);
+    inlasning(filnamn, text);
 
     berakna_histogram_abs(text, frekvens, antal);
     abs_till_rel(frekvens, relativFrekvens, antal);
-    plotta_historgram_rel(frekvens, relativFrekvens);
     tolkning(relativFrekvens);
-
+    plotta_historgram_rel(frekvens, relativFrekvens);
     
-    
-  return 0;
+    return 0;
 }
 
 // Funktionsdefinitioner:
-void berakna_histogram_abs(const string& in, int frekvens[], int &antal) {
-  antal = 0;
+void berakna_histogram_abs(const string &in, int frekvens[], int &antal) {
+    antal = 0;
 
-  //Sätter alla värden till 0
-  for (int i = 0; i < ANTAL_BOKSTAVER; i++) {
-    frekvens[i] = 0;
-  }
-
-  for (int i = 0; i < in.length(); i++) {
-
-    //Om en bokstav hittas på den platsen i strängen.
-    if (isalpha(in.at(i))) { // isalpha returnerar sant om tecknet är en bokstav
-      antal++;
-    }
-    
-    int index;
-
-    //Kolla versal
-    if (in.at(i) <= 'z' && in.at(i) >= 'a') {
-      index = in.at(i) - 'a';
-      frekvens[index]++;
+    // Sätter alla värden till 0
+    for (int i = 0; i < ANTAL_BOKSTAVER; i++)
+    {
+        frekvens[i] = 0;
     }
 
-    //Kolla gemen
-    if (in.at(i) <= 'Z' && in.at(i) >= 'A') {
-      index = in.at(i) - 'A';
-      frekvens[index]++;
+    for (int i = 0; i < in.length(); i++)
+    {
+
+        // Om en bokstav hittas på den platsen i strängen.
+        if (isalpha(in.at(i)))
+        { // isalpha returnerar sant om tecknet är en bokstav
+            antal++;
+        }
+
+        int index;
+
+        // Kolla versal
+        if (in.at(i) <= 'z' && in.at(i) >= 'a')
+        {
+            index = in.at(i) - 'a';
+            frekvens[index]++;
+        }
+
+        // Kolla gemen
+        if (in.at(i) <= 'Z' && in.at(i) >= 'A')
+        {
+            index = in.at(i) - 'A';
+            frekvens[index]++;
+        }
     }
-  }
 }
 
 void abs_till_rel(int frekvens[], double relativFrekvens[], int &antal) {
 
+    // Sätter alla värden till 0
+    for (int i = 0; i < ANTAL_BOKSTAVER; i++)
+    {
+        relativFrekvens[i] = 0;
+    }
+
+    for (int i = 0; i < ANTAL_BOKSTAVER; i++)
+    {
+        if (frekvens[i] > 0)
+        {
+            double procent = (double) frekvens[i] / antal * 100;
+            relativFrekvens[i] = procent;
+        }
+    }
+    cout << "Resultat for bokstaverna A-Z" << endl;
+    cout << "Totala antalet bokstaver: " << antal << endl;
 }
 
+void plotta_historgram_rel(int frekvens[], double relativFrekvens[]) {
+    for (int i = 0; i < ANTAL_BOKSTAVER; i++)
+    {
+        if (frekvens[i] > 0)
+        {
+
+            int trunkering = (relativFrekvens[i] / 0.5 + 0.5);
+
+            cout << char(i + 'A') << " ";
+
+            // Skriver ut *
+            for (int j = 0; j < trunkering; j++)
+            {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+}
+
+void tolkning(double relativFrekvens[]) {
+    double diff[ANTAL_SPRAK];
+    double summa[ANTAL_SPRAK];
+    string sprak[ANTAL_SPRAK] = {"engelska", "franska", "svenska", "tyska"};
+
+    // Nollställer
+    for (int i = 0; i < ANTAL_SPRAK; i++)
+    {
+        diff[i] = 0;
+        summa[i] = 0;
+    }
+
+    // Kvadrera skillnader
+    for (int i = 0; i < ANTAL_BOKSTAVER; i++)
+    {
+        for (int j = 0; j < ANTAL_SPRAK; j++)
+        {
+            diff[j] = TOLK_HJALP[j][i] - relativFrekvens[i];
+            summa[j] += diff[j] * diff[j];
+        }
+    }
+
+    // Olika språks kvadratsummor
+    cout << "engelska har kvadratsumman= " << summa[0] << endl;
+    cout << "franska har kvadratsumman= " << summa[1] << endl;
+    cout << "svenska har kvadratsumman= " << summa[2] << endl;
+    cout << "tyska har kvadratsumman= " << summa[3] << endl;
+
+    double min = summa[0];
+    int index = 0;
+
+    // Ger det rätta språket (kanske)
+    for (int i = 0; i < ANTAL_SPRAK; i++)
+    {
+        if (min > summa[i])
+        {
+            min = summa[i];
+            index = i;
+        }
+    }
+    cout << "Det ar mest troligt att spraket ar " << sprak[index] << endl;
+}
+
+void namn_pa_fil(string &filnamn) {
+    cout << "Ange filnamn: " << endl;
+    getline(cin, filnamn);
+
+    // Om ".txt" ej är i filnamnet, läggs det till
+    if (filnamn.rfind(".txt") == string::npos)
+    {
+        filnamn.append(".txt");
+    }
+}
+
+void inlasning(string filnamn, string &text) {
+    char tecken;
+
+    ifstream fin(filnamn.c_str());
+
+    if (!fin)
+    {
+        cout << "Filen hittas ej" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    while (fin.get(tecken))
+    {
+        text.push_back(tecken);
+    }
+}
--- Inlupp 4/Readme.md
+++ Inlupp 4/Readme.md
@@ -134,3 +134,152 @@ Y: 0
 Z: 0  
 
 ## Del B
+
+### Testdata 1
+Ange filnamn:  
+doyle  
+Resultat for bokstaverna A-Z  
+Totala antalet bokstaver: 12885  
+engelska har kvadratsumman= 1.75771  
+franska har kvadratsumman= 96.1956  
+svenska har kvadratsumman= 73.3371  
+tyska har kvadratsumman= 116.072  
+Det ar mest troligt att spraket ar engelska  
+A ****************  
+B ***  
+C *******  
+D ********  
+E ************************  
+F ****  
+G ***  
+H ************  
+I ***************  
+J   
+K **  
+L ********  
+M *****  
+N *************  
+O ***************  
+P ****  
+Q  
+R ***********  
+S *************  
+T ******************  
+U ******  
+V **  
+W ****  
+X  
+Y *****  
+Z  
+
+### Testdata 2
+Ange filnamn:  
+goethe  
+Resultat for bokstaverna A-Z  
+Totala antalet bokstaver: 14027  
+engelska har kvadratsumman= 107.856  
+franska har kvadratsumman= 85.205  
+svenska har kvadratsumman= 160.318  
+tyska har kvadratsumman= 1.04313  
+Det ar mest troligt att spraket ar tyska  
+A ***********  
+B ***  
+C *******  
+D *********  
+E *********************************  
+F ***  
+G *******  
+H ***********  
+I ******************  
+J  
+K **  
+L *******  
+M ******  
+N **********************  
+O ****  
+P *  
+Q  
+R ************  
+S **************  
+T ***********  
+U ********  
+V **  
+W ****  
+Y *  
+Z ***  
+
+### Testdata 3
+Ange filnamn:  
+lagerlof  
+Resultat for bokstaverna A-Z  
+Totala antalet bokstaver: 12221  
+engelska har kvadratsumman= 86.6804  
+franska har kvadratsumman= 145.722  
+svenska har kvadratsumman= 8.88258  
+tyska har kvadratsumman= 170.223  
+Det ar mest troligt att spraket ar svenska  
+A ***********************  
+B **  
+C ***  
+D ************  
+E *******************  
+F ****  
+G ********  
+H *******  
+I ********  
+J ***  
+K *******  
+L **********  
+M *******  
+N ******************  
+O *********  
+P ***  
+R ***************  
+S **************  
+T ******************  
+U ***  
+V ******  
+X  
+Y *  
+
+### Testdata 4
+Ange filnamn:   
+verne  
+Resultat for bokstaverna A-Z  
+Totala antalet bokstaver: 11417  
+engelska har kvadratsumman= 112.425  
+franska har kvadratsumman= 0.885142  
+svenska har kvadratsumman= 149.256  
+tyska har kvadratsumman= 90.491  
+Det ar mest troligt att spraket ar franska  
+A ****************  
+B ***  
+C *******  
+D ********  
+E ***********************************  
+F **  
+G **  
+H *  
+I **************  
+J *  
+K  
+L ***********  
+M ******  
+N ***************  
+O **********  
+P ******  
+Q **  
+R **************  
+S *****************  
+T **************  
+U ************  
+V ***   
+W  
+X *  
+Y *  
+Z  
+
+### Testdata 5
+Ange filnamn:   
+Lisa   
+Filen hittas ej  
